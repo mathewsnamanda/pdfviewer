e05a89f460cf59d5dc6873d2dd79784d1219a8f1
unknown <matcelous@gmail.com>
2022-07-14 08:46:16 +0300

    first commit

---
+++ pdfviewer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pdfviewer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
